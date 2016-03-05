2663a3076fbde1fd6dc01a70b4b02f5b7146ffca
Mobi-Host.com <github@mobi-host.com>
2016-03-06 05:26:50 +0600

    update
    update

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 class Tourizm {
-  function Tourizm($username = '', $password = '') {
+  function Tourizm($localhost = '', $username = '', $password = '', $database = '') {
+    $this->localhost = $localhost;
     $this->username = $username;
     $this->password = $password;
+    $this->database = $database;
+    $this->connection = mysqli_connect('localhost',$this->username,$this->password,$this->database);
   }
 }
