3f08ffa77748185df166352a027e8c68fb8abc7b
Mobi-Host.com <github@mobi-host.com>
2016-09-28 06:37:29 +0600

    up
    up

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ class Tourizm {
     $this->username = $username;
     $this->password = $password;
     $this->database = $database;
-    $this->connection = mysqli_connect('localhost',$this->username,$this->password,$this->database);
+    $function = 'mysqli_connect';
+    if ( function_exists($function) )
+    {
+      $this->connection = mysqli_connect('localhost',$this->username,$this->password,$this->database);
+    }
+    else
+    {
+      throw new Exception('Отсуствие функции '.$function);
+    }
   }
 }
